a87f33ef5939800779bbb7fc184b446ff33fd969
Jean-Marc CUSTOS <jean-marc.custos@ascentry.com>
2026-09-09 10:36:22 +0200

    TFS-140170 Ajout projet de tests InfectionTracker

---
+++ InfectionTracker.E2E.Tests/AbstractTestBase.cs
@@ -0,0 +1,20 @@
+﻿using Ascentry.E2E.Configurations;
+using Ascentry.E2E.Testing;
+using Microsoft.Extensions.Configuration;
+
+namespace InfectionTracker.E2E.Tests
+{
+    public abstract class AbstractTestBase : AscentryTestBase
+    {
+        static AbstractTestBase()
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("configuration.json", optional: true)
+                .AddUserSecrets<AbstractTestBase>(optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            TestConfiguration.Initialize(configuration);
+        }
+    }
+}
---
+++ InfectionTracker.E2E.Tests/ExpertRules/ExpertRuleTests.cs
@@ -0,0 +1,25 @@
+﻿using Ascentry.E2E.Contracts.Enums;
+using Ascentry.E2E.Contracts.ExpertRules;
+using Ascentry.E2E.Navigations.Menus;
+
+namespace InfectionTracker.E2E.Tests.ExpertRules
+{
+    public class ExpertRuleTests: AbstractTestBase
+    {
+
+        [Fact(DisplayName = "Execute an expert rule in test mode - Success - one row with one condition with one instruction")]
+        public async Task Should_Execute_Test_Mode_One_Row_One_Condition_One_Instruction()
+        {
+            await NavigationMenu.GoToAsync(InfectionTrackerMenu.Settings.Expertise.ExpertRules);
+
+            var expertRule = await ExpertRuleListPage.OpenAsync("101309-Générer un évènement");
+            await expertRule.VerifyEditModeAsync();
+
+            ExpertRuleResult result = await expertRule.ExecuteTestModeAsync(ExpertRuleTestModeContextTypeEnum.PatientPid, "P11161287623");
+            Assert.Single(result.RowResults);
+            Assert.True(result.RowResults[0].IsSuccess);
+            Assert.True(result.RowResults[0].RowConditionResults[0].IsSuccess);
+            Assert.True(result.RowResults[0].RowConditionResults[0].RowInstructionResults[0].IsSuccess);
+        }
+    }
+}
